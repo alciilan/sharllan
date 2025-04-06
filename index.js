a6db6c7e65df02da3dc5907c775d409d8c478f01
alciilan <alcilan20@gmail.com>
2025-04-06 09:58:57 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import AutoPrimeSite from "./App";
+import "./index.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <AutoPrimeSite />
+  </React.StrictMode>
+);
